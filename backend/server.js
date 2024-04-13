6efcba5409f3d4875e8200df14aa562b51bc6bf7
PopatRitik <popatritik2012@gmail.com>
2024-04-14 03:13:32 +0530

    job started

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,12 @@ import messageRoutes from "./routes/messageRoute.js";
 import {v2 as cloudinary} from "cloudinary";
 import bodyParser from 'body-parser';
 import {app,server} from './socket/socket.js';
+import job from './cron/cron.js';
 
 dotenv.config();
 
 connectDB();
+job.start();
 
 const PORT=process.env.PORT || 5000;
 const __dirname = path.resolve();
